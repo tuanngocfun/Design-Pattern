04c79772817547fb3206b4482727bec120b71b04
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 17:07:12 +0700

    Implement 'WithRequest' handlers to enhance modular data processing
    - Added 'WithRequest' handlers for Red and Violet color processing, separating DataFormattingRequest and DataDto handling.
    - This approach allows for more granular control and precise condition checks on the request attributes separately from the data, enhancing flexibility and scalability of our processing logic.
    - Contrast to 'WithoutRequest' handlers where the combined CompoundDataDto limits condition granularity and modularity in processing.

---
+++ ChainOfResponsibility/Mock/WithRequest/Handlers/VioletHandler1.cs
@@ -0,0 +1,20 @@
+﻿using ChainOfResponsibility.Mock.Models;
+using ChainOfResponsibility.Mock.Utilities;
+using ChainOfResponsibility.Mock.WithRequest.Bases;
+
+namespace ChainOfResponsibility.Mock.WithRequest.Handlers
+{
+    public class VioletHandler1 : VioletHandlerBase
+    {
+        protected override Task<DataDto> ExecuteAsync(DataFormattingRequest request, DataDto obj, Func<Task<DataDto>> next)
+        {
+            obj.Message = this.AddMessage(obj.Message);
+
+            // Do something
+            // Do something
+            // Do something
+
+            return next();
+        }
+    }
+}
