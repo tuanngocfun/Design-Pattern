0257642f21a07716b6cfd0eea65b2994c99b8495
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 14:51:27 +0700

    Implement Chain of Responsibility pattern with AbstractPipelineInvoker classes
    This commit introduces two abstract base classes, `AbstractPipelineInvoker<TRequest, THandled>` and `AbstractPipelineInvoker<THandled>`, which implement the Chain of Responsibility pattern. These classes provide a way to execute a sequence of handlers (implementing `IHandler<TRequest, THandled>` or `IHandler<THandled>`) on a request or object, respectively.
    
    The `AbstractPipelineInvoker<TRequest, THandled>` class takes a list of `IHandler<TRequest, THandled>` instances in its constructor and provides an `InvokePipeline` method to execute the handlers in the pipeline. The `AbstractPipelineInvoker<THandled>` class is similar but operates solely on objects of type `THandled`.
    
    Both classes include a virtual `Preprocess` method that allows subclasses to perform any preprocessing on the request/object before invoking the pipeline.

---
+++ ChainOfResponsibility/Core/AbstractPipelineInvoker.cs
@@ -0,0 +1,83 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public abstract class AbstractPipelineInvoker<TRequest, THandled> 
+        where TRequest : class 
+        where THandled : class
+    {
+        protected IList<IHandler<TRequest, THandled>> Pipeline { get; set; }
+
+        protected AbstractPipelineInvoker(IList<IHandler<TRequest, THandled>> pipeline)
+        {
+            Pipeline = pipeline;
+        }
+
+        protected THandled InvokePipeline(TRequest request, THandled obj)
+        {
+            var i = 0;
+            Func<THandled> callNext = default!;
+
+            callNext = () =>
+            {
+                if (i < Pipeline.Count)
+                {
+                    return Pipeline[i++].Handle(request, obj, callNext);
+                }
+                else
+                {
+                    return obj;
+                }
+            };
+
+            // Give subclasses a chance to preprocess the request and object before passing it to the pipeline
+            Preprocess(request, obj);
+
+            // Invoke the pipeline
+            return callNext.Invoke();
+        }
+
+        protected virtual void Preprocess(TRequest request, THandled obj)
+        {
+            // Default implementation is do nothing
+        }
+    }
+
+    public abstract class AbstractPipelineInvoker<THandled> 
+        where THandled : class
+    {
+        protected IList<IHandler<THandled>> Pipeline { get; set; }
+
+        protected AbstractPipelineInvoker(IList<IHandler<THandled>> pipeline)
+        {
+            Pipeline = pipeline;
+        }
+
+        protected THandled InvokePipeline(THandled obj)
+        {
+            var i = 0;
+            Func<THandled> callNext = default!;
+
+            callNext = () =>
+            {
+                if (i < Pipeline.Count)
+                {
+                    return Pipeline[i++].Handle(obj, callNext);
+                }
+                else
+                {
+                    return obj;
+                }
+            };
+
+            // Give subclasses a chance to preprocess the object before passing it to the pipeline
+            Preprocess(obj);
+
+            // Invoke the pipeline
+            return callNext.Invoke();
+        }
+
+        protected virtual void Preprocess(THandled obj)
+        {
+            // Default implementation is do nothing
+        }
+    }
+}
