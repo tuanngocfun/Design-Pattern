9f9594624e68627bfde63e0c63439e3ae64642d8
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:10:56 +0700

    Add models for data processing in Chain of Responsibility pattern
    - Added `CompoundDataDto` to link data with its formatting requests.
    - Introduced `DataDto` to encapsulate the actual data fields.
    - Defined `DataFormattingRequest` to specify formatting details like color and number.

---
+++ ChainOfResponsibility/Mock/Models/DataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ChainOfResponsibility.Mock.Models
+{
+    public class DataDto
+    {
+        public string Field1 { get; set; } = string.Empty;
+
+        public string? Field2 { get; set; } = default;
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
