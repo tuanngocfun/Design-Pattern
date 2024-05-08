b79d32e92131947bc744df2346578b95cf84de67
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:17:14 +0700

    Add AddMessage extension method to enhance object logging
    - Introduced the AddMessage extension method in ObjectExtensions within the ChainOfResponsibility.Mock.Utilities namespace.
    - This method appends the object's type name to a given message, aiding in debugging and logging scenarios.

---
+++ ChainOfResponsibility/Mock/Utilities/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainOfResponsibility.Mock.Utilities
+{
+    public static class ObjectExtensions
+    {
+        public static string AddMessage(this object @this, string message)
+        {
+            var msg = @this.GetType().Name;
+            return string.IsNullOrWhiteSpace(message) 
+                ? msg 
+                : message + "\n" + msg;
+        }
+    }
+}
