a34b8c30f8aa056db33fdff801af73b216e1f981
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 14:51:57 +0700

    Implement Asynchronous Chain of Responsibility pattern with AbstractAsyncPipelineInvoker classes
    This commit introduces two abstract base classes, `AbstractAsyncPipelineInvoker<TRequest, THandled>` and `AbstractAsyncPipelineInvoker<THandled>`, which implement an asynchronous version of the Chain of Responsibility pattern. These classes provide a way to execute a sequence of asynchronous handlers (implementing `IAsyncHandler<TRequest, THandled>` or `IAsyncHandler<THandled>`) on a request or object, respectively.
    
    The `AbstractAsyncPipelineInvoker<TRequest, THandled>` class takes a list of `IAsyncHandler<TRequest, THandled>` instances in its constructor and provides an `InvokePipelineAsync` method to execute the handlers in the pipeline asynchronously. The `AbstractAsyncPipelineInvoker<THandled>` class is similar but operates solely on objects of type `THandled`.
    
    Both classes include a virtual `Preprocess` method that allows subclasses to perform any asynchronous preprocessing on the request/object before invoking the pipeline.
    
    The asynchronous execution is achieved by using `async`/`await` keywords and a recursive lambda expression to iteratively invoke the handlers in the pipeline.

---
+++ ChainOfResponsibility/Core/AbstractAsyncPipelineInvoker.cs
@@ -0,0 +1,85 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public abstract class AbstractAsyncPipelineInvoker<TRequest, THandled> 
+        where TRequest : class 
+        where THandled : class
+    {
+        protected IList<IAsyncHandler<TRequest, THandled>> Pipeline { get; set; }
+
+        protected AbstractAsyncPipelineInvoker(IList<IAsyncHandler<TRequest, THandled>> pipeline)
+        {
+            Pipeline = pipeline;
+        }
+
+        protected async Task<THandled> InvokePipelineAsync(TRequest request, THandled obj)
+        {
+            var i = 0;
+            Func<Task<THandled>> callNext = default!;
+
+            callNext = () =>
+            {
+                if (i < Pipeline.Count)
+                {
+                    return Pipeline[i++].HandleAsync(request, obj, callNext);
+                }
+                else
+                {
+                    return Task.FromResult(obj);
+                }
+            };
+
+            // Give subclasses a chance to preprocess the request and object before passing it to the pipeline
+            await Preprocess(request, obj);
+
+            // Invoke the pipeline
+            return await callNext.Invoke();
+        }
+
+        protected virtual Task Preprocess(TRequest request, THandled obj)
+        {
+            // Default implementation is do nothing
+            return Task.CompletedTask;
+        }
+    }
+
+    public abstract class AbstractAsyncPipelineInvoker<THandled> 
+        where THandled : class
+    {
+        protected IList<IAsyncHandler<THandled>> Pipeline { get; set; }
+
+        protected AbstractAsyncPipelineInvoker(IList<IAsyncHandler<THandled>> pipeline)
+        {
+            Pipeline = pipeline;
+        }
+
+        protected async Task<THandled> InvokePipelineAsync(THandled obj)
+        {
+            var i = 0;
+            Func<Task<THandled>> callNext = default!;
+
+            callNext = () =>
+            {
+                if (i < Pipeline.Count)
+                {
+                    return Pipeline[i++].HandleAsync(obj, callNext);
+                }
+                else
+                {
+                    return Task.FromResult(obj);
+                }
+            };
+
+            // Give subclasses a chance to preprocess the object before passing it to the pipeline
+            await Preprocess(obj);
+
+            // Invoke the pipeline
+            return await callNext.Invoke();
+        }
+
+        protected virtual Task Preprocess(THandled obj)
+        {
+            // Default implementation is do nothing
+            return Task.CompletedTask;
+        }
+    }
+}
