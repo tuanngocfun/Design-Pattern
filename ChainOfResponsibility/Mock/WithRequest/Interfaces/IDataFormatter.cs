08ad47f65d81357748b214c5d2f022868adbac18
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 17:09:57 +0700

    Implement separate and combined data/request formatters
    - Added 'WithRequest' DataFormatter for separate handling of DataFormattingRequest and DataDto, enhancing modularity and flexibility in condition checks and data manipulations.
    - Contrast with 'WithoutRequest' DataFormatter where the compound data object combines both request and data, simplifying the handler structure but reducing flexibility.

---
+++ ChainOfResponsibility/Mock/WithRequest/Interfaces/IDataFormatter.cs
@@ -0,0 +1,9 @@
+﻿using ChainOfResponsibility.Mock.Models;
+
+namespace ChainOfResponsibility.Mock.WithRequest.Interfaces
+{
+    public interface IDataFormatter
+    {
+        Task FormatAsync(DataFormattingRequest request, DataDto dto);
+    }
+}
