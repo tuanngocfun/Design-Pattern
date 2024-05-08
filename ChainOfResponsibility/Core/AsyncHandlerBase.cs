8ec1fd5981710e85e53881dbef4aff5f2400a368
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 14:38:15 +0700

    define core contracts and classes implement the logic to check if they should handle a request and to pass it to the next handler if not.

---
+++ ChainOfResponsibility/Core/AsyncHandlerBase.cs
@@ -0,0 +1,35 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public abstract class AsyncHandlerBase<TRequest, THandled> : IAsyncHandler<TRequest, THandled>
+        where TRequest : class
+        where THandled : class
+    {
+        public Task<THandled> HandleAsync(TRequest request, THandled obj, Func<Task<THandled>> next)
+        {
+            return ShouldHandle(request, obj) ? ExecuteAsync(request, obj, next) : next.Invoke();
+        }
+
+        protected virtual bool ShouldHandle(TRequest request, THandled obj)
+        {
+            return true;
+        }
+
+        protected abstract Task<THandled> ExecuteAsync(TRequest request, THandled obj, Func<Task<THandled>> next);
+    }
+
+    public abstract class AsyncHandlerBase<THandled> : IAsyncHandler<THandled>
+        where THandled : class
+    {
+        public Task<THandled> HandleAsync(THandled obj, Func<Task<THandled>> next)
+        {
+            return ShouldHandle(obj) ? ExecuteAsync(obj, next) : next.Invoke();
+        }
+
+        protected virtual bool ShouldHandle(THandled obj)
+        {
+            return true;
+        }
+
+        protected abstract Task<THandled> ExecuteAsync(THandled obj, Func<Task<THandled>> next);
+    }
+}
---
+++ ChainOfResponsibility/Core/IAsyncHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public interface IAsyncHandler<in TRequest, THandled> 
+        where TRequest : class 
+        where THandled : class
+    {
+        Task<THandled> HandleAsync(TRequest request, THandled obj, Func<Task<THandled>> next);
+    }
+
+    public interface IAsyncHandler<THandled> 
+        where THandled : class
+    {
+        Task<THandled> HandleAsync(THandled obj, Func<Task<THandled>> next);
+    }
+}
