392a88afade7fb09acfbf3b9fee35c808e2cbf48
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:23:12 +0700

    Implement Red color handlers in Chain of Responsibility pattern
    - Added RedHandlerBase as a base class for handlers that process when color is Red.
    - Introduced RedHandler1 to append messages and continue the chain.
    - Introduced RedHandler2 with an additional check on the Number property (>200).
    - Introduced RedHandler3 to handle cases where Field1 is empty or null.
    - Utilized the AddMessage extension method for consistent message formatting.

---
+++ ChainOfResponsibility/Mock/WithoutRequest/Handlers/RedHandler2.cs
@@ -0,0 +1,25 @@
+﻿using ChainOfResponsibility.Mock.Models;
+using ChainOfResponsibility.Mock.Utilities;
+using ChainOfResponsibility.Mock.WithoutRequest.Bases;
+
+namespace ChainOfResponsibility.Mock.WithoutRequest.Handlers
+{
+    public class RedHandler2 : RedHandlerBase
+    {
+        protected override bool ShouldHandle(CompoundDataDto obj)
+        {
+            return base.ShouldHandle(obj) && obj.Request.Number > 200;
+        }
+
+        protected override Task<CompoundDataDto> ExecuteAsync(CompoundDataDto obj, Func<Task<CompoundDataDto>> next)
+        {
+            obj.Data.Message = this.AddMessage(obj.Data.Message);
+
+            // Do something
+            // Do something
+            // Do something
+
+            return next();
+        }
+    }
+}
---
+++ ChainOfResponsibility/Mock/WithoutRequest/Handlers/RedHandler3.cs
@@ -0,0 +1,25 @@
+﻿using ChainOfResponsibility.Mock.Models;
+using ChainOfResponsibility.Mock.Utilities;
+using ChainOfResponsibility.Mock.WithoutRequest.Bases;
+
+namespace ChainOfResponsibility.Mock.WithoutRequest.Handlers
+{
+    public class RedHandler3 : RedHandlerBase
+    {
+        protected override bool ShouldHandle(CompoundDataDto obj)
+        {
+            return base.ShouldHandle(obj) && string.IsNullOrEmpty(obj.Data.Field1);
+        }
+
+        protected override Task<CompoundDataDto> ExecuteAsync(CompoundDataDto obj, Func<Task<CompoundDataDto>> next)
+        {
+            obj.Data.Message = this.AddMessage(obj.Data.Message);
+
+            // Do something
+            // Do something
+            // Do something
+
+            return next();
+        }
+    }
+}
