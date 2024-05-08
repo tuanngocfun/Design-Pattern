dda870e99d71f8733f41f04b082c389d94fc3940
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 17:10:37 +0700

    In Program.cs, set up an instance of your pipeline invoker.     Add instances of your handlers to the pipeline.     Create a test request and pass it through the pipeline.     Print or log the results to verify that the chain works as expected.

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,36 @@
+﻿using ChainOfResponsibility.Core;
+using ChainOfResponsibility.Mock.Enums;
+using ChainOfResponsibility.Mock.WithoutRequest.Formatters;
+using ChainOfResponsibility.Mock.WithoutRequest.Handlers;
+using ChainOfResponsibility.Mock.Models;
+
+//var handlers = new List<IAsyncHandler<DataFormattingRequest, DataDto>>();
+var handlers = new List<IAsyncHandler<CompoundDataDto>>();
+var formatter = new DataFormatter(handlers);
+
+handlers.Add(new RedHandler2());
+handlers.Add(new VioletHandler1());
+handlers.Add(new RedHandler1());
+handlers.Add(new RedHandler3());
+handlers.Add(new VioletHandler2());
+
+var request = new DataFormattingRequest()
+{
+    Color = Color.Red,
+    Number = 555
+};
+var dto = new DataDto()
+{
+    Message = "Hello World!",
+    Field1 = "Lorem Ipsum"
+};
+var compountDto = new CompoundDataDto()
+{
+    Request = request,
+    Data = dto
+};
+
+//await formatter.FormatAsync(request, dto);
+await formatter.FormatAsync(compountDto);
+
+Console.WriteLine(dto.Message);
