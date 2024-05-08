b023a2ae31ed41fc8b52eb0aa9244279a7141efd
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 14:33:03 +0700

    define core contracts and classes

---
+++ ChainOfResponsibility/Core/HandlerBase.cs
@@ -0,0 +1,35 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public abstract class HandlerBase<TRequest, THandled> : IHandler<TRequest, THandled> 
+        where TRequest : class 
+        where THandled : class
+    {
+        public THandled Handle(TRequest request, THandled obj, Func<THandled> next)
+        {
+            return ShouldHandle(request, obj) ? Execute(request, obj, next) : next.Invoke();
+        }
+
+        protected virtual bool ShouldHandle(TRequest request, THandled obj)
+        {
+            return true;
+        }
+
+        protected abstract THandled Execute(TRequest request, THandled obj, Func<THandled> next);
+    }
+
+    public abstract class HandlerBase<THandled> : IHandler<THandled> 
+        where THandled : class
+    {
+        public THandled Handle(THandled obj, Func<THandled> next)
+        {
+            return ShouldHandle(obj) ? Execute(obj, next) : next.Invoke();
+        }
+
+        protected virtual bool ShouldHandle(THandled obj)
+        {
+            return true;
+        }
+
+        protected abstract THandled Execute(THandled obj, Func<THandled> next);
+    }
+}
---
+++ ChainOfResponsibility/Core/IHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace ChainOfResponsibility.Core
+{
+    public interface IHandler<in TRequest, THandled> 
+        where TRequest : class 
+        where THandled : class
+    {
+        THandled Handle(TRequest request, THandled obj, Func<THandled> next);
+    }
+
+    public interface IHandler<THandled> 
+        where THandled : class
+    {
+        THandled Handle(THandled obj, Func<THandled> next);
+    }
+}
