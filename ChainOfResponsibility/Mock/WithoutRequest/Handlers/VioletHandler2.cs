9a67af677b0209bc5861a88b58303ba1dc4133a6
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:53:21 +0700

    Implement Violet color handlers in Chain of Responsibility pattern
    - Added VioletHandler1 to append messages and continue the chain for Violet-colored data.
    - Introduced VioletHandler2 with an additional check on Field2 being default before processing.

---
+++ ChainOfResponsibility/Mock/WithoutRequest/Handlers/VioletHandler2.cs
@@ -0,0 +1,25 @@
+﻿using ChainOfResponsibility.Mock.Models;
+using ChainOfResponsibility.Mock.Utilities;
+using ChainOfResponsibility.Mock.WithoutRequest.Bases;
+
+namespace ChainOfResponsibility.Mock.WithoutRequest.Handlers
+{
+    public class VioletHandler2 : VioletHandlerBase
+    {
+        protected override bool ShouldHandle(CompoundDataDto obj)
+        {
+            return base.ShouldHandle(obj) && obj.Data.Field2 == default;
+        }
+
+        protected override Task<CompoundDataDto> ExecuteAsync(CompoundDataDto obj, Func<Task<CompoundDataDto>> next)
+        {
+            obj.Data.Message = this.AddMessage(obj.Data.Message);
+
+            // Do something
+            // Do something
+            // Do something
+
+            return next();
+        }
+    }
+}
