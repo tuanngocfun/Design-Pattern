14beb72691cfc5964dd1dc0da38179e01a2555ee
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:15:21 +0700

    Add Color enum to define standard color options
    - Introduced a Color enum in the ChainOfResponsibility.Mock.Enums namespace.
    - Defined color options ranging from None to Violet to support diverse color coding needs.

---
+++ ChainOfResponsibility/Mock/Enums/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.Mock.Enums
+{
+    public enum Color
+    {
+        None,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet,
+    }
+}
