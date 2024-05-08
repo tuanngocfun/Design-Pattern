27376734f785278210856c3db3a8c1ff808db7dd
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-08 16:41:11 +0700

    Implement DataFormatter in Chain of Responsibility pattern
    - Introduced DataFormatter class extending AbstractAsyncPipelineInvoker to process CompoundDataDto objects.
    - Implemented IDataFormatter interface in DataFormatter with FormatAsync method to initiate pipeline invocation.
    - Added Preprocess method in DataFormatter to reset Data properties before processing.
    note:
        Handlers like RedHandlerBase focus on conditions in Request (DataFormattingRequest inside CompoundDataDto).
        DataFormatter manages the whole CompoundDataDto, including invoking handlers and initializing data, hence it works directly with CompoundDataDto.

---
+++ ChainOfResponsibility/Mock/WithoutRequest/Formatters/DataFormatter.cs
@@ -0,0 +1,28 @@
+﻿using ChainOfResponsibility.Core;
+using ChainOfResponsibility.Mock.Models;
+using ChainOfResponsibility.Mock.WithoutRequest.Interfaces;
+
+namespace ChainOfResponsibility.Mock.WithoutRequest.Formatters
+{
+    public class DataFormatter : AbstractAsyncPipelineInvoker<CompoundDataDto>, IDataFormatter
+    {
+        public DataFormatter(IList<IAsyncHandler<CompoundDataDto>> handlers) : base(handlers)
+        {
+
+        }
+
+        public Task FormatAsync(CompoundDataDto dto)
+        {
+            return InvokePipelineAsync(dto);
+        }
+
+        protected override Task Preprocess(CompoundDataDto obj)
+        {
+            obj.Data.Field1 = string.Empty;
+            obj.Data.Field2 = string.Empty;
+            obj.Data.Message = string.Empty;
+
+            return Task.CompletedTask;
+        }
+    }
+}
